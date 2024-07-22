ed05f083bf1044a997cbedecfc351c42b519f71d
Yem Daro <100412991+darororo@users.noreply.github.com>
2024-07-22 16:50:37 +0700

    set up routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get("/posts/{id}", function($id) {
+    return response('Post ' . $id);
+})->where('id', '[0-9]+');
+
+Route::get('/search', function(Request $request) {
+    dd($request->name);
 });
