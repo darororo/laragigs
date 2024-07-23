fe9dff1157e3643240d83cce945bd4afd23d347c
Yem Daro <100412991+darororo@users.noreply.github.com>
2024-07-23 14:02:32 +0700

    Listing factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
+
+        Listing::factory(6)->create();
 
         // User::factory()->create([
         //     'name' => 'Test User',
