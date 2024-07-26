8e25cce10081109ed98e26872d2399e0b5aeae1d
Yem Daro <100412991+darororo@users.noreply.github.com>
2024-07-26 16:18:48 +0700

    User Registration

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ListingController;
 use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 Route::get('/', [ListingController::class, 'index']);
 
@@ -25,3 +26,9 @@ Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 // single listing
 Route::get("/listings/{listing}",[ListingController::class, 'show'] );
 
+
+//Show register create form
+Route::get('/register', [UserController::class, 'create']);
+
+// Create New User
+Route::post('/users', [UserController::class, 'store']);
