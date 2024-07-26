ba34d5f471b7c1a8cb53f1f512d2906d3fd1ec39
Yem Daro <100412991+darororo@users.noreply.github.com>
2024-07-26 16:34:21 +0700

    User logout

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,6 @@ Route::get('/register', [UserController::class, 'create']);
 
 // Create New User
 Route::post('/users', [UserController::class, 'store']);
+
+// Log user out
+Route::post('/logout', [UserController::class, 'logout']);
